2ba91647548fb633dbf5955116dd0cd84151e1fd
matteuusz Stochel <mateuszstochel1995@gmail.com>
2020-03-09 22:13:02 +0100

    Added models controllers and migration changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "Mati", password: "password")
+User.create(username: "Mati2", password: "password")
+User.create(username: "Mati3", password: "password")
+User.create(username: "Mati4", password: "password")
+User.create(username: "Mati5", password: "password")
